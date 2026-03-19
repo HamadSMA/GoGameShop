385f6b6247f21dc871761258101572b467e1ee51
Hamad Alaslani <hamadsmma@gmail.com>
2026-03-20 00:27:31 +0300

    chore: Add seed data and modified Game model

--- Backend/src/GoGameShop.Api/Models/Game.cs
+++ Backend/src/GoGameShop.Api/Models/Game.cs
@@ -1,6 +1,3 @@
-using System;
-using Microsoft.AspNetCore.SignalR;
-
 namespace GoGameShop.Api.Models;
 
 public class Game
@@ -8,7 +5,9 @@ public class Game
     public Guid Id { get; set; }
     public required string Name { get; set; }
     public Genre Genre { get; set; } = null!;
+    public Guid GenreId { get; set; }
     public Rating Rating { get; set; } = null!;
+    public Guid RatingId { get; set; }
     public DateOnly ReleaseDate { get; set; }
     public decimal Price { get; set; }
     public required string Description { get; set; }
--- Backend/src/GoGameShop.Api/Models/Rating.cs
+++ Backend/src/GoGameShop.Api/Models/Rating.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace GoGameShop.Api.Models;
 
 public class Rating
