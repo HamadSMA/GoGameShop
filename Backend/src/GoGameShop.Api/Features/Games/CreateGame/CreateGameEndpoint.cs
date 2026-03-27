9ea40d1e21db143d7290c06ea24321e966675be4
Hamad Alaslani <hamadsmma@gmail.com>
2026-03-27 04:50:19 +0300

    refactor: simplify game creation logic
    Replaced `GoGameShopData` with `GoGameShopContext` to streamline database interactions. Removed redundant genre and rating validation to rely on database constraints instead. Updated HTTP datastore entries with new GUIDs.

--- Backend/src/GoGameShop.Api/Features/Games/CreateGame/CreateGameEndpoint.cs
+++ Backend/src/GoGameShop.Api/Features/Games/CreateGame/CreateGameEndpoint.cs
@@ -5,34 +5,22 @@ public static class CreateGameEndpoint
     public static void MapCreateGame(this IEndpointRouteBuilder app)
     {
         // POST /games
-        app.MapPost("/", (GoGameShopData data, CreateGameDto gameDto) =>
+        app.MapPost("/", (GoGameShopContext dbContext, CreateGameDto gameDto) =>
         {
-            var genre = data.GetGenre(gameDto.GenreId);
-            var rating = data.GetRating(gameDto.RatingId);
-            if (genre is null)
-            {
-                return Results.BadRequest("Invalid genre id");
-            }
-
-            if (rating is null)
-            {
-                return Results.BadRequest("Invalid genre id");
-            }
-
-
+            
             Game game = new()
             {
                 Name = gameDto.Name,
-                Genre = genre,
-                GenreId = genre.Id,
-                Rating = rating,
-                RatingId = rating.Id,
+                GenreId = gameDto.GenreId,
+                RatingId = gameDto.RatingId,
                 ReleaseDate = gameDto.ReleaseDate,
                 Price = gameDto.Price,
                 Description = gameDto.Description
             };
 
-            data.AddGame(game);
+            dbContext.Add(game);
+            dbContext.SaveChanges();
+            
             return Results.CreatedAtRoute(EndpointNames.GetGame, new { id = game.Id }, new GameDetailsDto(
                 game.Id,
                 game.Name,
