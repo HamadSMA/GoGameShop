44c588ff1685cc6e327ee782a5b9f9853cd63dc6
Hamad Alaslani <hamadsmma@gmail.com>
2026-03-27 05:54:42 +0300

    refactor: include related data in game retrieval
    Updated `/games` endpoint to use `GoGameShopContext` with `Include` for related entities (`Genre` and `Rating`) to ensure joined data retrieval. Added `AsNoTracking` for read-only optimization and improved query performance.

--- Backend/src/GoGameShop.Api/Features/Games/GetGames/GetGamesEndpoint.cs
+++ Backend/src/GoGameShop.Api/Features/Games/GetGames/GetGamesEndpoint.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace GoGameShop.Api.Features.Games.GetGames;
 
 public static class GetGamesEndpoint
@@ -5,13 +7,19 @@ public static class GetGamesEndpoint
     public static void MapGetGames(this IEndpointRouteBuilder app)
     {
         // GET /games
-        app.MapGet("/", (GoGameShopData data) => data.GetGames.Select(game => new GameSummaryDto(
-            game.Id,
-            game.Name,
-            game.Genre!.Name,
-            game.Rating!.Name,
-            game.Price,
-            game.ReleaseDate
-        )));
+        app.MapGet("/",
+            (GoGameShopContext dbContext) => dbContext.Games
+                .Include(game => game.Genre)
+                .Include(game => game.Rating)
+                .Select(game => new GameSummaryDto(
+                    game.Id,
+                    game.Name,
+                    game.Genre!.Name,
+                    game.Rating!.Name,
+                    game.Price,
+                    game.ReleaseDate
+                    ))
+                .AsNoTracking());
+        
     }
 }
