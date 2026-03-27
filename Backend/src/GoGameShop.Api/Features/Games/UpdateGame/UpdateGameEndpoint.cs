779b3b5663e96c4b2be83e3e5090e29ef61b19cc
Hamad Alaslani <hamadsmma@gmail.com>
2026-03-27 05:29:42 +0300

    refactor: streamline game update endpoint logic
    Replaced `GoGameShopData` with `GoGameShopContext` for consistency in database interactions. Simplified logic by removing redundant genre and rating validation, relying on constraints instead. Updated HTTP data with new game details for testing.

--- Backend/src/GoGameShop.Api/Features/Games/UpdateGame/UpdateGameEndpoint.cs
+++ Backend/src/GoGameShop.Api/Features/Games/UpdateGame/UpdateGameEndpoint.cs
@@ -1,7 +1,3 @@
-using System;
-using GoGameShop.Api.Data;
-using GoGameShop.Api.Models;
-
 namespace GoGameShop.Api.Features.Games.UpdateGame;
 
 public static class UpdateGameEndpoint
@@ -9,37 +5,27 @@ public static class UpdateGameEndpoint
     public static void MapUpdateGame(this IEndpointRouteBuilder app)
     {
         // PUT /games/{id}
-        app.MapPut("/{id}", (Guid id, GoGameShopData data, UpdateGameDto gameDto) =>
+        app.MapPut("/{id}", (Guid id, GoGameShopContext dbContext, UpdateGameDto gameDto) =>
         {
-            Game? game = data.GetGame(id);
-            Genre? genre = data.GetGenre(gameDto.GenreId);
-            Rating? rating = data.GetRating(gameDto.RatingId);
+            var game = dbContext.Games.Find(id);
 
             if (game is null)
             {
                 return Results.NotFound("Game not found");
             }
 
-            if (genre is null)
-            {
-                return Results.BadRequest("Invalid genre id");
-            }
-
-            if (rating is null)
-            {
-                return Results.BadRequest("invalid rating id");
-            }
-
             game.Name = gameDto.Name;
-            game.Genre = genre;
-            game.GenreId = genre.Id;
-            game.Rating = rating;
-            game.RatingId = rating.Id;
+            game.GenreId = gameDto.GenreId;
+            game.RatingId = gameDto.RatingId;
             game.ReleaseDate = gameDto.ReleaseDate;
             game.Price = gameDto.Price;
             game.Description = gameDto.Description;
-
+            
+            dbContext.SaveChanges();
+            
             return Results.NoContent();
+
         });
+
     }
 }
